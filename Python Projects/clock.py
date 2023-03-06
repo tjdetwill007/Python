e656d5ad5f286d0fb4da50388596b1d7a98ee566
tjdetwill007 <tjdetwill007@gmail.com>
2023-03-06 21:35:44 +0530

    New clock app

---
+++ Python Projects/clock.py
@@ -0,0 +1,16 @@
+from tkinter import *
+from tkinter.ttk import *
+from time import strftime
+
+root=Tk()
+root.title('clock')
+def time():
+    string=strftime('%H:%M:%S %p')
+    lable.config(text=string)
+    lable.after(1000,time)
+
+lable= Label(root, font=('Arial', 80), background='Blue', foreground='Black')
+lable.pack(anchor = 'center')
+
+time()
+mainloop()
